b74863e7109ce969fd44750463c381fe78f1cda7
boorgoof <federico.31mene@icloud.com>
2025-05-25 12:29:21 +0200

    data_house

--- scheduling_problem/combine_data.py
+++ scheduling_problem/combine_data.py
@@ -164,7 +164,202 @@ def get_data():
     return data
 
 
+def get_data_house():
+    
+    data = {}
+    
+    solar_predictor = SolarProductionPredictor(
+    model_path="../weather_pv_conversion/output/model/best_estimator_model.pkl",
+    scaler_path="../weather_pv_conversion/output/model/scaler.pkl"
+    )
 
+
+    # 1. https://www.expressvpn.com/it/blog/il-consumo-energetico-della-tecnologia/?srsltid=AfmBOorBF1XynXRfEISxCWEpMMz4LLAI-2oxi2d1u-fjlvkUJGdnhW8D
+
+    machine_energy_usage = [
+        ("Portable Heater", 1.5*1000, 2),
+        ("Air Conditioner (240V)", 1.8*1000, 4),
+        ("Pedestal Fan", 0.03*1000, 8),
+        ("Ceiling Fan", 0.05*1000, 6),
+        ("Incandescent Bulbs (60W)", 0.75*1000, 8),
+        ("Clock", 0.1*1000, 24),
+
+        # Bathroom
+        ("Electric Water Heater", 0.52*1000, 3),
+        ("Electric Toothbrush", 0.003*1000, 5/60),
+        ("Hair Dryer", 1.5*1000, 20/60),
+        ("Curling Iron", 0.15*1000, 10/60),
+
+        # Bedroom
+        ("Electric Blanket (Queen)", 0.09*1000, 3),
+        ("Night Light (4W)", 0.12*1000 10),
+        ("Smartphone Charger", 0.001*1000, 24),
+        ("Alarm Clock", 0.008*1000, 24),
+
+        # Kitchen
+        ("Oven", 2.3*1000, 0.5),
+        ("Cooktop", 1.5*1000, 0.5),
+        ("Microwave", 1.44*1000, 0.25),
+        ("Kettle", 0.11*1000, 10/60),
+        ("Coffee Maker/Warmer", 0.4*1000, 1),
+        ("Dishwasher (normal cycle)", 1.1*1000, 2),
+        ("Toaster Oven", 0.75*1000, 5/60),
+        ("Refrigerator + Freezer (17 cu. ft.)", 0.04*1000, 24),
+        ("Air Fryer (1500W)", 1.5*1000, 20/60),
+
+        # Laundry
+        ("Dryer", 3.0*1000, 2/7),
+        ("Conventional Washing Machine", 2.3*1000, 2/7),
+        ("Vacuum Cleaner", 0.75*1000, 2/7),
+        ("Robot Vacuum", 0.007*1000, 45/60),
+        ("Iron", 1.08*1000, 40/60/7),
+
+        # Living Room
+        ("LED TV (average)", 0.095*1000, 3),  # average between 0.071 and 0.12
+        ("Cable Box", 0.01*1000, 3),
+        ("Media Player (Roku)", 0.002*1000, 3),
+        ("Game Console", 0.07*1000, 2),
+        ("Speakers (2 x 25W)", 0.05*1000, 3),
+        ("Stereo", 0.05*1000, 2),
+        ("Radio/CD Player", 0.02*1000 1.5),
+        ("Halogen Floor Lamp", 0.02*1000, 6),
+
+        # Home Office
+        ("Wi-Fi Router", 0.01*1000, 24),
+        ("Desktop Computer", 0.06*1000, 2),
+        ("Laptop", 0.05*1000, 2),
+        ("LCD Monitor (17\")", 0.04*1000, 2),
+        ("Printer", 0.001*1000, 10/60/7),
+
+        # Outdoor/Garage
+        ("Pool Pump", 0.56*1000, 6),
+        ("Hot Tub (1500W)", 1.5*1000, 0.25),
+        ("Electric Car (charging)", 0.49*1000, 7.5),
+        ("Electric Bike (500Wh) (charging)", 0.37*1000, 3.5)  # Assuming 1h charge/day
+    ]
+
+    print("Machine names:")
+    for i, (name, _, _) in enumerate(machine_energy_usage, start=1):
+        print(f"{i}. {name}")
+    
+
+    MACHINES = len(machine_energy_usage) # Number of machines
+    MAX_JOB_N = 24  # Maximum number of jobs per machine
+    T_MAX = 24  # Number of time periods (e.g., 48 half-hours in a day)
+    
+    print(f"Machine number:", MACHINES)
+    print("Max jobs per machine:", MAX_JOB_N)
+    print("Max time period:", T_MAX)
+    
+    data["T_MAX"] = T_MAX
+    data["MACHINES"] = MACHINES
+
+    # Define sets
+    I = list(range(1, MACHINES + 1 )) # machines 
+    T = list(range(1, T_MAX + 1)) # time periods TODO
+    J = list(range(1, MAX_JOB_N + 1)) # jobs TODO
+    
+    data["T"] = T
+    data["I"] = I
+    data["J"] = J
+    
+    # Energy parameters
+    # e_i: energy consumption when machine i is running
+    
+    e = {}
+    for i, (_, energy, _) in enumerate(machine_energy_usage, start=1):
+        e[i] = energy
+    print("e =", e)
+    data["e"] = e
+
+    # f_i: additional energy consumed when machine i starts TODO
+    f = {i: int(e[i] * (1/6)) for i in e}
+    print("f =", f)
+    data["f"] = f
+
+    # d_i: duration of job i on machine i
+    d = {}
+    for i, (_, _, usage) in enumerate(machine_energy_usage, start=1):
+        d[i] = usage
+    print("d =", d)
+    data["d"] = d
+
+ 
+    # 2. SOLAR PANELS
+    # p_t: energy produced at time t by one unit of power
+    print("\n--------------------------------------\nSOLAR PANELS\n")
+    predictions_df = solar_predictor.predict(start_date_str="2023-07-25")
+    p = {}
+    for idx, (timestamp, row) in enumerate(predictions_df.iterrows(), start=1):
+        p[idx] = float(row['predicted_production'])
+        if (p[idx] < 0): p[idx] = 0
+        
+    data["p"] = p
+    print("\n\nSolar production in the day 2023-07-25:", p)
+    
+
+    # 3. GENERATED DATA
+    # Cost parameters
+    # https://www.mosaikosrl.net/batterie-accumulo-fotovoltaico/
+    B = 5000 # B: battery capacity  
+    c_b = 2.530 # Cost per battery
+    # https://www.ecodirect.com/Canadian-Solar-CS6X-300P-300W-36V-PV-Panel-p/canadian-solar-cs6x-300p.htm?srsltid=AfmBOor_kd4mknwa-Am9K9m7VYG55_jnXMM3QTP7aTw2Y2qCChJ9GuL7
+    c_p = 290   # Cost per unit of power: Canadian Solar CS6X-300P (inverter SMA_America_SB7000TL_US240V cost neglected)
+    # https://tariffe.segugio.it/guide-e-strumenti/domande-frequenti/quanto-costa-un-kwh-di-energia-elettrica.aspx#:~:text=Il%20prezzo%20dell'energia%20elettrica%20oggi%20%C3%A8%20pari%20a%200,si%20applica%20ai%20clienti%20vulnerabili.
+    c_e = 0.16053/1000 # Cost of energy 
+    
+
+    data["B"] = B
+    data["c_b"] = c_b
+    data["c_p"] = c_p
+    data["c_e"] = c_e
+    
+
+    # m_t: maximum energy available at time t TODO
+    mmm = {t: random.randint(800000, 1000000) for t in T}
+    data["mmm"] = mmm
+
+    # n_i: number of jobs required for machine i 
+    n_jobs = {}
+    for i, (_, _, _) in enumerate(machine_energy_usage, start=1):
+        n_jobs[i] = 1
+    data["n_jobs"] = n_jobs
+
+    # c_i: cooldown period for machine i TODO
+    c = {i: 1 for i in I}
+    data["c"] = c
+    
+    # THRESHOLD_FOR_JOB_J
+    THRESHOLD_FOR_JOB_J_AND_I = {(i,j): 24 for i in I for j in J} # Time limit for each job TODO
+    data["THRESHOLD_FOR_JOB_J_AND_I"] = THRESHOLD_FOR_JOB_J_AND_I
+
+
+    # Sets of dependencies and shared resources
+    M_dependencies = []  # Pairs of machines where the second depends on the first
+    M_shared = []  # Groups of machines that share resources and cannot run simultaneously
+    silent_periods = {}  # Periods when certain machines must be off
+    
+    data["M_dependencies"] = M_dependencies
+    data["M_shared"] = M_shared
+    data["silent_periods"] = silent_periods
+    
+    
+    # Print the generated data for reference
+    print("\n--------------------------------------\nGenerated Parameters: \n")
+    print(f"Cost per battery (c_b): {c_b}")
+    print(f"Cost per power unit (c_p): {c_p}")
+    print(f"Cost of energy taken from outside (c_e): {c_e}")
+    print(f"Battery capacity (B): {B}")
+    print(f"Max energy available (mmm): {mmm}")
+    print(f"Job durations (d): {d}")
+    print(f"Required jobs per machine (n_jobs): {n_jobs}")
+    print(f"Machine cooldown periods (c): {c}")
+    print(f"Machine dependencies: {M_dependencies}")
+    print(f"Shared resource groups: {M_shared}")
+    print(f"Silent periods: {silent_periods}")
+    print("--------------------------------------\n")
+
+    return data
 
 
 """
